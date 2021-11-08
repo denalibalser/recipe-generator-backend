781c3222755e9bf1eb3474a9f7d75f3f15766021
denalibalser <denali.balser167@gmail.com>
2021-11-07 17:41:00 -0800

    add comment to seed file and add all recipes to controller index action

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RecipesController < ApplicationController
 
     def index 
-        
+        @recipes = Recipe.all
     end 
 
     def show 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# TEST SEED:
 recipes = Recipe.create([{title: 'The Best of England Salad', image: 'https://spoonacular.com/recipeImages/663225-312x231.jpg', usedIngredientCount: 3, missedIngredientCount: 5, missedIngredients: ['lemon juice'], usedIngredients: ['tahini']}])
