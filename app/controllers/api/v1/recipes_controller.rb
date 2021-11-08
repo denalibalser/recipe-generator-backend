54f673ac22d2be186ecb5b780c2f0d5211f5d5e8
denalibalser <denali.balser167@gmail.com>
2021-11-07 17:29:59 -0800

    switch to pg database, add recipes migration table and migrate

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::RecipesController < ApplicationController
 
     def index 
-
+        
     end 
 
     def show 
-    
+
     end 
 
 end
--- db/migrate/20210927222224_create_recipes.rb
+++ db/migrate/20210927222224_create_recipes.rb
@@ -1,7 +1,13 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
-    
+      t.text :title
+      t.text :image
+      t.integer :usedIngredientCount
+      t.integer :missedIngredientCount
+      t.text :missedIngredients, array: true, default: []
+      t.text :usedIngredients, array: true, default: []
+
 
       t.timestamps
     end
