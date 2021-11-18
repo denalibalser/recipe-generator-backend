c30a83ef906b56f16543d3862ff2782283efe709
denalibalser <denali.balser167@gmail.com>
2021-11-18 10:55:50 -0800

    add namespaces to routes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::RecipesController < ApplicationController
 
     def index 
         @recipes = Recipe.all
+        render json: { status: 201, recipes: @recipes } 
     end 
 
     def show 
